c17e04a3f7ce63b6d2d8ac52dbced88f2945c7cc
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-04-09 13:31:41 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 import gspread
 from google.oauth2.service_account import Credentials
+
+# Import python built-in module math used for basic math operations like: square root, exponents etc.
+import math
+
+# Import statistics Library used to calculate basic statistic like: arithmetic mean, median, spread, bell curce and interpolate
+import statistics
+
+# Import NumPy https://numpy.org/doc/stable/user/absolute_beginners.html
+import numpy as np
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,8 +23,11 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('aircraft_data')
 
+
 multirole_fighter = SHEET.worksheet('multirole_fighter')
 
 data = multirole_fighter.get_all_values()
 
 print(data)
+
+-------------------------------------------------------------------------------
