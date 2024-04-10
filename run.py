4d8decb507273e8b9b3d77b058af52ae9eeb8e64
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-04-10 19:48:41 +0000

    Add selection_main_menu function

--- run.py
+++ run.py
@@ -4,6 +4,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+#Import OS Library
+import os
+
 # Import colorama for colored command line text (slightly below middle of page) https://www.studytonight.com/python-howtos/how-to-print-colored-text-in-python#:~:text=You%20can%20use%20the%20Colorama,for%20colored%20text%20in%20Python.
 #  ANSI escape codes. and/with colorama python library
 # I installed colorama with the following command (type in command line): - pip3 install colorama for Python 3 and pip install colorama for older versions of Python
@@ -37,10 +40,55 @@ data = multirole_fighter.get_all_values()
 # print(data)
 
 #------------------------------------------------------------------------------
+
 def esc(code):
     """ Function somehow necessary for colorama
     """
     return f'\033[{code}m'
+
+def main_menu_select():
+    """ Handles selection from main menu
+    """
+    print('MAIN MENU')
+    print('1. Add data')
+    print('2. Edit data')
+    print('3. Delete data')
+    print('4. Meta data')
+    print('5. Bell curve')
+    print('6. Inbetween points')
+    print('7. HELP')
+    print('8. QUIT PROGRAM\n')
+
+    while True:
+        selection_main_menu = input('\nPlease select an option by entering a number between 1-6:\n')
+        if selection_main_menu == '1':
+            get_airplane_data()
+            
+        elif selection_main_menu == '2':
+            edit_data()
+            
+        elif selection_main_menu == '3':
+            delete_data()
+            
+        elif selection_main_menu == '4':
+            create_meta_data_table()
+            
+        elif selection_main_menu == '5':
+            create_bell_curve_graph()
+
+        elif selection_main_menu == '6':
+            calc_inbetween_outside_point()
+
+        elif selection_main_menu == '7':
+            help()
+
+        #Abort the current running process
+        elif selection_main_menu == '8':
+            os.abort()
+
+        else:
+            print('Invalid choice, please enter a number between 1-6,\n')
+            continue
 
 
 def get_airplane_data():
@@ -53,6 +101,7 @@ def get_airplane_data():
 
     Returns: airplane_data list (str and float) - incomplete user input data for one airplane.
     """
+
 
     # Sub menu - Add data
     print('In the next step you will be asked to enter three out of the\nfollowing five aircraft parameters:\nWing span, Aspect ratio, Wing area, Max takeoff weight, Wing loading.\n')
@@ -67,7 +116,8 @@ def get_airplane_data():
     print('7. Wing span, Aspect ratio, ' + esc('238;2;9') + 'Wing area,'  + esc(0) + ' ' + esc('238;2;9') + 'Max takeoff weight,'  + esc(0) + ' Wing loading')
     print('8. Wing span, Aspect ratio, ' + esc('238;2;9') + 'Wing area,' + esc(0) + ' Max takeoff weight, ' + esc('238;2;9') + 'Wing loading\n'  + esc(0))
     print('Or type 0 to return to main menu\n')
-    input('Please select an option by entering a number between 0-8:')
+    input('Please select an option by entering a number between 0-8:\n')
+
 
 def validate_airplane_data():
     """
@@ -81,6 +131,7 @@ def validate_airplane_data():
     Argumemts: airplane_data list (str and float) - incomplete user input data for one airplane.
     Returns: true or false
     """
+
 
 def calc_wing_span():
     """ Calculates Wing span based on two values in the airplane_data list[].
@@ -165,6 +216,7 @@ def uppdate_dependent_airplane_data():
     Returns: airplane_data list (str and float) - completed user input data for one airplane.
     """
 
+
 def edit_data():
     """ Edit the relevant worksheet with the data provided
     """
@@ -188,10 +240,6 @@ def select_and_pull_airplane_data_from_worksheet():
     and returns the data as a list of lists.
     """
 
-
-def Input_case():
-    """ x
-    """
 
 def calc_mean():
     """ Calculate mean (Arithmetic mean) for a parameter in airplane_data[].
@@ -241,6 +289,16 @@ def create_meta_data_table():
 
     print("Meta data - Please select an option by entering a number between 0-x:")
 
+
+def create_bell_curve_graph():
+    """ Calculate mean (Arithmetic mean) for a parameter in airplane_data[].
+    
+    
+    Argumemts: airplane_data_select_parameter[]
+    Returns: 
+    """
+
+
 def calc_bell_curve():
     """ Calculate mean (Arithmetic mean) for a parameter in airplane_data[].
     
@@ -252,7 +310,7 @@ def calc_bell_curve():
     # numpy.random.normal(loc=0.0, scale=1.0, size=None)
 
 
-def calc_inbetween_point():
+def calc_inbetween_outside_point():
     """ Calculate (interpolate and extrapolate) value for a  for a value in airplane_data[].
 
     https://numpy.org/doc/stable/reference/generated/numpy.interp.html
@@ -263,8 +321,7 @@ def calc_inbetween_point():
     # numpy.interp(x, xp, fp, left=None, right=None, period=None)[source]
     # Note - The datas independent variable needs to be sorted so that it is increasing!
 
-    print('Please choose an airplane parameter whose value you want to evaluate/estimaten\nbetween (interpolate) or outside (extrapolate) of it's data points')
-
+    print("Please choose an airplane parameter whose value you want to evaluate/estimaten\nbetween (interpolate) or outside (extrapolate) of it's data points")
     print('1. Wing span')
     print('2. Aspect ratio')
     print('3. Wing area')
@@ -288,27 +345,11 @@ def help():
 
 def main():
     """ Run all program functions
-    
-        Parameters:
-        Requests user to input a number between 1-5
-        If statement executes a function dependent on input
-        1: place_order()
-        2: view_live_orders()
-        3: exits system using sys.exit()
-        Else requests the user tries again
 
     Argumemts:
     Returns:
     """
-    print('MAIN MENU')
-    print('1. Add data')
-    print('2. Edit data')
-    print('3. Delete data')
-    print('4. Meta data')
-    print('5. Inbetween points')
-    print('6. HELP\n')
-
-    input('Please select an option by entering a number between 1-6:')
+    main_menu_select()
 
 
 # Welcome message
